0a0d902d4d09975ed5899bcba3888727f07a42a4
Freeze777 <freezefrancis@gmail.com>
2016-09-08 11:09:39 +0530

    removed bin files

--- git_push.sh
+++ git_push.sh
@@ -1,9 +1,14 @@
 git add *
 git rm -r --cached *.pem
+git rm -r --cached *.pyc
 git rm -r --cached *.pkl
 git rm -r --cached *.csv
 git rm -r --cached *.npy
 git rm -r --cached *.npz
-git rm -r --cached *.gl
+git rm -r --cached *.sidx
+git rm -r --cached *.frame_idx
+git rm -r --cached *.0000
+git rm -r --cached *.ini
+git rm -r --cached *.bin
 git commit -a
 git push https://github.com/Freeze777/ML-Codes.git
